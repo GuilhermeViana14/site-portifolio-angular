a575873fde96c5813d378ca83be52be323945005
GuilhermeViana14 <guilhermevianafilgueiras@gmail.com>
2024-08-19 13:31:26 -0300

    adicionado a parte da lista de projetos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PaginaCurriculoComponent } from './componentes/pagina-curriculo/pagina-curriculo.component';
 import { PaginaSobreMimComponent } from './componentes/pagina-sobre-mim/pagina-sobre-mim.component';
-
+import { PaginaProjetosComponent } from './componentes/pagina-projetos/pagina-projetos.component';
 const routes: Routes = [
 
-  {path: 'curriculo', component: PaginaCurriculoComponent},
   {path:'sobreMim', component: PaginaSobreMimComponent},
+  {path:'projetos', component: PaginaProjetosComponent},
 ];
 
 @NgModule({
